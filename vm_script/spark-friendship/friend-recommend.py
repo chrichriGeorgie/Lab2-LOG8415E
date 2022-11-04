5cba373ddfe9d792b11be6503bba0942bf844c68
louis <louisalexhebert@gmail.com>
2022-11-04 16:53:54 -0400

    rename steps for map reduce and simplify parameters

--- vm_script/spark-friendship/friend-recommend.py
+++ vm_script/spark-friendship/friend-recommend.py
@@ -10,6 +10,13 @@ def map_friends_and_commons(user, friends):
     commons = [(pair, 1) for pair in itertools.permutations(friends, 2)]
     return connecteds + commons
 
+def map_user_potential_friend_count(row):
+    user = row[0][0]
+    potentialFriend = row[0][1]
+    count = row[1]
+    return (user, potentialFriend, count)
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: friend-recommend <file>", file=sys.stderr)
@@ -22,19 +29,15 @@ if __name__ == "__main__":
 
     numberOfRecommendations = 10
 
-    rdd1 = spark.sparkContext.textFile(sys.argv[1])
-    rdd2 = rdd1.map(lambda line: line.split('\t'))
+    lines = spark.sparkContext.textFile(sys.argv[1])
+    userFriends = lines.map(lambda line: line.split('\t'))
 
-    rdd3 = rdd2.flatMap(lambda x: map_friends_and_commons(x[0], x[1].split(',')))
+    pairCounts = userFriends.flatMap(lambda x: map_friends_and_commons(x[0], x[1].split(','))) \
+    .reduceByKey(add) \
+    .filter(lambda row: row[1] > 0)
 
-    rdd4 = rdd3.reduceByKey(add)
+    userPotentialFriend = pairCounts.map(lambda x: map_user_potential_friend_count(x))
 
-    rdd5 = rdd4.filter(lambda x: x[1] > 0)
-    rdd5.toDF().show()
-
-    rdd6 = rdd5.map(lambda x: (x[0][0], x[0][1], x[1]))
-    #rdd6.toDF().show()
-
-    rdd7 = rdd6.filter(lambda x: x[0] == '1').sortBy(lambda x: x[2], ascending=False).toDF().show(numberOfRecommendations)
+    rdd7 = userPotentialFriend.filter(lambda x: x[0] == '1').sortBy(lambda x: x[2], ascending=False).toDF().show(numberOfRecommendations)
 
     spark.stop()
