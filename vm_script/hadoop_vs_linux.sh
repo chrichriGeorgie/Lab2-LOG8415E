c5946eded015730489ddc73abf0b4cbc4c4cc1a4
Christophe St-Georges <christophe.st-georges@polymtl.ca>
2022-11-03 20:42:59 -0400

    Created script for the Hadoop vs Spark experiement and made the experiment scripts run sequentially

---
+++ vm_script/hadoop_vs_linux.sh
@@ -0,0 +1,29 @@
+#Building hadoop container
+cd hadoop-wordcount
+docker build -t hadoop-log8415 .
+
+#Starting hadoop container
+cd ..
+docker run -d --name hadoop-log8415 --mount type=bind,source="$PWD"/resources,target=/opt/hadoop/resources hadoop-log8415
+
+#Experiment header
+echo "HADOOP VS LINUX"
+echo "======================================"
+printf "\n"
+
+#Run WordCount Experiment on Hadoop
+echo "Running Hadoop WordCount on pg4300.txt"
+echo "Time Taken:"
+time docker exec -i hadoop-log8415 bash -c "hadoop jar ./share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.1.jar wordcount input output > hadoop-output.txt"
+printf "\n"
+
+#Run WordCount Experiment on Linux
+
+echo "Running Linux WordCount on pg4300.txt"
+echo "Time Taken:"
+time docker exec -i hadoop-log8415 bash -c "cat ./resources/pg4300.txt | tr ' ' '\n' | sort | uniq -c > linux-output.txt"
+printf "\n"
+
+#Stoping and removing hadoop vs linux containers and images
+docker stop hadoop-log8415
+docker rm hadoop-log8415
