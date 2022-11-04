8129843522d67271b154b5a66cf46bb6cba565a7
jeromeCleris <jerome.cleris@polymtl.ca>
2022-11-04 16:35:26 -0400

    friend-recommendation finished

--- vm_script/spark-friendship/friend-recommend.py
+++ vm_script/spark-friendship/friend-recommend.py
@@ -1,6 +1,5 @@
 import sys
 import itertools
-import re
 from operator import add
 from pyspark.sql import SparkSession
 from collections import Counter
@@ -10,8 +9,6 @@ def map_friends_and_commons(user, friends):
     connecteds = [((user, friend), alreadyConnectedCount) for friend in friends]
     commons = [(pair, 1) for pair in itertools.permutations(friends, 2)]
     return connecteds + commons
-
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -23,20 +20,21 @@ if __name__ == "__main__":
         .appName("PythonFriendRecommend")\
         .getOrCreate()
 
+    numberOfRecommendations = 10
+
     rdd1 = spark.sparkContext.textFile(sys.argv[1])
     rdd2 = rdd1.map(lambda line: line.split('\t'))
-    #rdd2.toDF().show()
 
     rdd3 = rdd2.flatMap(lambda x: map_friends_and_commons(x[0], x[1].split(',')))
-    #rdd3.toDF().show()
 
-    rdd4 = rdd3.reduceByKey(lambda total, current: total + current)
-    #rdd4.toDF().show()
+    rdd4 = rdd3.reduceByKey(add)
 
     rdd5 = rdd4.filter(lambda x: x[1] > 0)
     rdd5.toDF().show()
 
-    rdd6 = rdd5.map(lambda x: (x[0][1], (x[1], x[0][1])))
-    rdd6.toDF().show()
+    rdd6 = rdd5.map(lambda x: (x[0][0], x[0][1], x[1]))
+    #rdd6.toDF().show()
+
+    rdd7 = rdd6.filter(lambda x: x[0] == '1').sortBy(lambda x: x[2], ascending=False).toDF().show(numberOfRecommendations)
 
     spark.stop()
