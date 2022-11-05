4b8064a585ab3ba2368b8c748ed5552bac2e06ab
louis <louisalexhebert@gmail.com>
2022-11-04 20:55:04 -0400

    spark friendship works with faster processing

--- vm_script/spark-friendship/friend-recommend.py
+++ vm_script/spark-friendship/friend-recommend.py
@@ -2,7 +2,8 @@ import sys
 import itertools
 from operator import add
 from pyspark.sql import SparkSession
-from collections import Counter
+from pyspark.sql.functions import col
+import time
 
 def map_friends_and_commons(user, friends):
     alreadyConnectedCount = -9999999999
@@ -12,10 +13,21 @@ def map_friends_and_commons(user, friends):
 
 def map_user_potential_friend_count(row):
     user = row[0][0]
-    potentialFriend = row[0][1]
+    potentialFriend = int(row[0][1])
     count = row[1]
     return (user, potentialFriend, count)
 
+
+def get_user_recommendations(dataFrame, user, maxRecommendations):
+    potentialFriendsString = "\t"
+
+    dataFrame.createOrReplaceTempView("table")
+    rows = spark.sql("select user, potentialFriend from table where user == " + user + " ORDER BY count desc, potentialFriend asc LIMIT " + str(maxRecommendations)).collect()
+    if(len(rows) > 0):
+        for row in rows:
+            potentialFriendsString += str(row[1]) + " "
+
+    print(user + potentialFriendsString)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -27,17 +39,29 @@ if __name__ == "__main__":
         .appName("PythonFriendRecommend")\
         .getOrCreate()
 
+    startTime = time.time()
     numberOfRecommendations = 10
 
     lines = spark.sparkContext.textFile(sys.argv[1])
     userFriends = lines.map(lambda line: line.split('\t'))
-
     pairCounts = userFriends.flatMap(lambda x: map_friends_and_commons(x[0], x[1].split(','))) \
     .reduceByKey(add) \
     .filter(lambda row: row[1] > 0)
 
-    userPotentialFriend = pairCounts.map(lambda x: map_user_potential_friend_count(x))
+    userPotentialFriend = pairCounts.map(lambda x: map_user_potential_friend_count(x)).toDF(['user', 'potentialFriend', 'count'])
 
-    rdd7 = userPotentialFriend.filter(lambda x: x[0] == '1').sortBy(lambda x: x[2], ascending=False).toDF().show(numberOfRecommendations)
+    get_user_recommendations(userPotentialFriend, '924', numberOfRecommendations)
+    get_user_recommendations(userPotentialFriend, '8941', numberOfRecommendations)
+    get_user_recommendations(userPotentialFriend, '8942', numberOfRecommendations)
+    get_user_recommendations(userPotentialFriend, '9019', numberOfRecommendations)
+    get_user_recommendations(userPotentialFriend, '9020', numberOfRecommendations)
+    get_user_recommendations(userPotentialFriend, '9021', numberOfRecommendations)
+    get_user_recommendations(userPotentialFriend, '9022', numberOfRecommendations)
+    get_user_recommendations(userPotentialFriend, '9990', numberOfRecommendations)
+    get_user_recommendations(userPotentialFriend, '9992', numberOfRecommendations)
+    get_user_recommendations(userPotentialFriend, '9993', numberOfRecommendations)
+
+    endTime = time.time()
+    print("Execution took ", endTime-startTime, " sec")
 
     spark.stop()
