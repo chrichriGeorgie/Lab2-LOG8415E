73f662175d34060b9b9ffbcb3b295817f8f04517
louis <louisalexhebert@gmail.com>
2022-11-04 21:16:09 -0400

    move number of recommandations inside function, clean imports

--- vm_script/spark-friendship/friend-recommend.py
+++ vm_script/spark-friendship/friend-recommend.py
@@ -2,9 +2,9 @@ import sys
 import itertools
 from operator import add
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
 import time
 
+# 
 def map_friends_and_commons(user, friends):
     alreadyConnectedCount = -9999999999
     connecteds = [((user, friend), alreadyConnectedCount) for friend in friends]
@@ -18,11 +18,12 @@ def map_user_potential_friend_count(row):
     return (user, potentialFriend, count)
 
 
-def get_user_recommendations(dataFrame, user, maxRecommendations):
+def get_user_recommendations(dataFrame, user):
+    numberOfRecommendations = 10
     potentialFriendsString = "\t"
 
     dataFrame.createOrReplaceTempView("table")
-    rows = spark.sql("select user, potentialFriend from table where user == " + user + " ORDER BY count desc, potentialFriend asc LIMIT " + str(maxRecommendations)).collect()
+    rows = spark.sql("select user, potentialFriend from table where user == " + user + " ORDER BY count desc, potentialFriend asc LIMIT " + str(numberOfRecommendations)).collect()
     if(len(rows) > 0):
         for row in rows:
             potentialFriendsString += str(row[1]) + " "
@@ -50,16 +51,17 @@ if __name__ == "__main__":
 
     userPotentialFriend = pairCounts.map(lambda x: map_user_potential_friend_count(x)).toDF(['user', 'potentialFriend', 'count'])
 
-    get_user_recommendations(userPotentialFriend, '924', numberOfRecommendations)
-    get_user_recommendations(userPotentialFriend, '8941', numberOfRecommendations)
-    get_user_recommendations(userPotentialFriend, '8942', numberOfRecommendations)
-    get_user_recommendations(userPotentialFriend, '9019', numberOfRecommendations)
-    get_user_recommendations(userPotentialFriend, '9020', numberOfRecommendations)
-    get_user_recommendations(userPotentialFriend, '9021', numberOfRecommendations)
-    get_user_recommendations(userPotentialFriend, '9022', numberOfRecommendations)
-    get_user_recommendations(userPotentialFriend, '9990', numberOfRecommendations)
-    get_user_recommendations(userPotentialFriend, '9992', numberOfRecommendations)
-    get_user_recommendations(userPotentialFriend, '9993', numberOfRecommendations)
+    # Friend recommandations for different users
+    get_user_recommendations(userPotentialFriend, '924')
+    get_user_recommendations(userPotentialFriend, '8941')
+    get_user_recommendations(userPotentialFriend, '8942')
+    get_user_recommendations(userPotentialFriend, '9019')
+    get_user_recommendations(userPotentialFriend, '9020')
+    get_user_recommendations(userPotentialFriend, '9021')
+    get_user_recommendations(userPotentialFriend, '9022')
+    get_user_recommendations(userPotentialFriend, '9990')
+    get_user_recommendations(userPotentialFriend, '9992')
+    get_user_recommendations(userPotentialFriend, '9993')
 
     endTime = time.time()
     print("Execution took ", endTime-startTime, " sec")
